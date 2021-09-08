7e3ed42cc323fb16108d729f0f743e6b6423b342
Andrew Hambly <ahambly@practiceenginesystems.onmicrosoft.com>
2021-09-08 14:55:49 +0100

    Include unlinked credit in MTD transfer

--- db/PE.NominalLedger.Db/Stored Procedures/MTD/pe_NL_MTD_Extract.sql
+++ db/PE.NominalLedger.Db/Stored Procedures/MTD/pe_NL_MTD_Extract.sql
@@ -13,7 +13,7 @@ SELECT D.DebtTranIndex, CAST(0 as bit), D.RowVer
 FROM tblTranDebtor D 
 INNER JOIN tblTranNominalOrgs O ON D.PracID = O.PracID
 LEFT OUTER JOIN tblTranNominalMTD M ON D.DebtTranIndex = M.DebtTranIndex
-WHERE D.DebtTranType IN (3,4,6,14) AND D.RowVer BETWEEN @MinRowVer AND @MaxRowVer AND M.DebtTranIndex IS NULL AND O.NLTransfer = 1
+WHERE D.DebtTranType IN (3,4,6,14,26) AND D.RowVer BETWEEN @MinRowVer AND @MaxRowVer AND M.DebtTranIndex IS NULL AND O.NLTransfer = 1
 
 UPDATE tblTranNominalMTDControl
 SET LastExtract = GETDATE(), MaxRowVer = @MaxRowVer
